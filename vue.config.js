b0873497767ba6f0ee691d9990a5f95ae9177efa
lilin <1242981132@qq.com>
2023-10-22 21:48:47 +0800

    commit2

--- vue.config.js
+++ vue.config.js
@@ -1,5 +1,119 @@
-const { defineConfig } = require('@vue/cli-service')
-module.exports = defineConfig({
-  transpileDependencies: true,
-  lintOnSave: false,
-})
+const path = require('path')
+const webpack = require('webpack')
+const CompressionPlugin = require("compression-webpack-plugin")
+function resolve(dir) {
+  return path.join(__dirname, dir)
+}
+
+// vue.config.js
+module.exports = {
+  /*
+    Vue-cli3:
+    Crashed when using Webpack `import()` #2463
+    https://github.com/vuejs/vue-cli/issues/2463
+   */
+  // 如果你不需要生产环境的 source map，可以将其设置为 false 以加速生产环境构建。
+  productionSourceMap: false,
+  //打包app时放开该配置
+  //publicPath:'./',
+  configureWebpack: config => {
+    //生产环境取消 console.log
+    if (process.env.NODE_ENV === 'production') {
+      config.optimization.minimizer[0].options.terserOptions.compress.drop_console = true
+      config.optimization.splitChunks.maxAsyncRequests = 30
+      config.optimization.splitChunks.maxInitialRequests = 20
+    }
+  },
+  chainWebpack: (config) => {
+    config.plugins.delete('prefetch')
+    //只保留中文语言包
+    config.plugin('ContextReplacementPlugin').use(webpack.ContextReplacementPlugin, [/moment[/\\]locale$/, /zh-cn/])
+
+    config.resolve.alias
+      .set('@$', resolve('src'))
+      .set('@api', resolve('src/api'))
+      .set('@assets', resolve('src/assets'))
+      .set('@comp', resolve('src/components'))
+      .set('@views', resolve('src/views'))
+
+      	//修改vue.config.js
+        config.set('externals', {
+          vue: 'Vue',
+          axios: 'axios',
+          'tinymce': 'tinymce'
+        })
+
+    //生产环境，开启js\css压缩
+    if (process.env.NODE_ENV === 'production') {
+      
+        config.plugin('compressionPlugin').use(new CompressionPlugin({
+          test: /\.(js|css|less|jpg|png)$/, // 匹配文件名
+          threshold: 10240, // 对超过10k的数据压缩
+          deleteOriginalAssets: false // 不删除源文件
+        }))
+        config.plugin('chunkPlugin').use(webpack.optimize.LimitChunkCountPlugin,[{
+          maxChunks: 5, 
+          minChunkSize: 100
+        }])
+
+    }
+
+    // 配置 webpack 识别 markdown 为普通的文件
+    config.module
+      .rule('markdown')
+      .test(/\.md$/)
+      .use()
+      .loader('file-loader')
+      .end()
+
+    // 编译vxe-table包里的es6代码，解决IE11兼容问题
+    config.module
+      .rule('vxe')
+      .test(/\.js$/)
+      .include
+        .add(resolve('node_modules/vxe-table'))
+        .add(resolve('node_modules/vxe-table-plugin-antd'))
+        .end()
+      .use()
+      .loader('babel-loader')
+      .end()
+
+  },
+
+  css: {
+    loaderOptions: {
+      less: {
+        modifyVars: {
+          /* less 变量覆盖，用于自定义 ant design 主题 */
+          'primary-color': '#CC148A',
+          'link-color': '#3055BA',
+          'border-radius-base': '4px',
+        },
+        javascriptEnabled: true,
+      }
+    }
+  },
+
+  devServer: {
+    port: 3000,
+    proxy: {
+     /* '/api': {
+        target: 'https://mock.ihx.me/mock/5baf3052f7da7e07e04a5116/antd-pro', //mock API接口系统
+        ws: false,
+        changeOrigin: true,
+        pathRewrite: {
+          '/jeecg-boot': ''  //默认所有请求都加了jeecg-boot前缀，需要去掉
+        }
+      },*/
+      '/jeecg-boot': {
+        target: 'http://localhost:8080', //请求本地 需要jeecg-boot后台项目
+        // target: 'http://123.60.222.136:20001/', //请求本地 需要jeecg-boot后台项目
+        // target: 'http://123.60.222.136:20001/jeecg-boot/', //请求本地 需要jeecg-boot后台项目
+        ws: false,
+        changeOrigin: true
+      },
+    }
+  },
+
+  lintOnSave: undefined
+}
